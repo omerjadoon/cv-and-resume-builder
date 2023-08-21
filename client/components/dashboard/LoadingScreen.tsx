4541c53d5bce273b9c1360c4dce1867407859e6e
omerjadoon <omerjadoon1@gmail.com>
2023-08-21 14:44:33 +0200

    error fix

--- client/components/dashboard/LoadingScreen.tsx
+++ client/components/dashboard/LoadingScreen.tsx
@@ -1,11 +1,17 @@
+import styles from './ResumePreview.module.scss';
+
 export const LoadingScreen = () => {
     return (
-        <div className="loading-screen">
-            <div className="dot"></div>
-            <div className="dot"></div>
-            <div className="dot"></div>
-            <div className="dot"></div>
-            <div className="dot"></div>
+        <div>
+            <p>Loading Results...! Please Wait</p>
+        <div className={styles.loadingscreen}>
+            <div className={styles.dot}></div>
+            <div className={styles.dot}></div>
+            <div className={styles.dot}></div>
+            <div className={styles.dot}></div>
+            <div className={styles.dot}></div>
+            
+        </div>
         </div>
     );
 };
