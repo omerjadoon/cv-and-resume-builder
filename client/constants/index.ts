7f9d5f2a05b5d4f5fddae1cd53b805c578b22c5a
omerjadoon <omerjadoon1@gmail.com>
2023-09-13 03:13:17 +0200

    updated translation

--- client/constants/index.ts
+++ client/constants/index.ts
@@ -9,14 +9,14 @@ export const VALID_URL_REGEX = /[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}
 export const FILENAME_TIMESTAMP = 'DDMMYYYYHHmmss';
 
 // Links
-export const DOCS_URL = 'https://docs.rxresu.me';
-export const DONATION_URL = 'https://paypal.me/RajaRajanA';
-export const TRANSLATE_URL = 'https://translate.rxresu.me/';
-export const DIGITALOCEAN_URL = 'https://pillai.xyz/digitalocean';
+export const DOCS_URL = 'https://';
+export const DONATION_URL = 'https://';
+export const TRANSLATE_URL = 'h';
+export const DIGITALOCEAN_URL = '';
 export const REDDIT_URL = 'https://www.reddit.com/r/reactiveresume/';
-export const GITHUB_URL = 'https://github.com/AmruthPillai/Reactive-Resume';
-export const PRODUCT_HUNT_URL = 'https://www.producthunt.com/posts/reactive-resume-v3';
-export const GITHUB_ISSUES_URL = 'https://github.com/AmruthPillai/Reactive-Resume/issues/new/choose';
+export const GITHUB_URL = 'https://github.com/';
+export const PRODUCT_HUNT_URL = 'https://www.producthunt.com/';
+export const GITHUB_ISSUES_URL = 'https://github.com';
 
 // Default Error Message
 export const DEFAULT_ERROR_MESSAGE =
