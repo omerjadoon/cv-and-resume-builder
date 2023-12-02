3067ad2480daecae5a54ad0841830928cedd74c6
omerjadoon <omerjadoon1@gmail.com>
2023-12-02 06:41:25 +0100

    google auth

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,41 +3,38 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import cookieParser from 'cookie-parser';
+import fs from 'fs';
 
 import { AppModule } from './app.module';
 
-import fs from 'fs';
-
 const bootstrap = async () => {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+const httpsOptions = {
+  key: fs.readFileSync('/etc/letsencrypt/live/hiresafarijobs.com/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/hiresafarijobs.com/fullchain.pem'),
+};
+const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+  httpsOptions,
+});
   const configService = app.get(ConfigService);
-  const environment = configService.get<string>('NODE_ENV') || 'development';
+
+  const appUrl = configService.get<string>('app.url');
 
   // Middleware
+  app.enableCors({ origin: '*', credentials: true });
   app.enableShutdownHooks();
   app.use(cookieParser());
+
 
   // Pipes
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   // Server Port
+  // const port = configService.get<number>('app.port');
   const port = process.env.PORT || 3100;
   await app.listen(port);
 
-  Logger.log(`🚀 Server is up and running on port ${port} in ${environment} environment!`);
-
-  if (environment === 'production') {
-    // Use Let's Encrypt encryption in production
-    const httpsOptions = {
-      key: fs.readFileSync('/etc/letsencrypt/live/hiresafarijobs.com/privkey.pem'),
-      cert: fs.readFileSync('/etc/letsencrypt/live/hiresafarijobs.com/fullchain.pem'),
-    };
-    app.enableCors({ origin: '*', credentials: true });
-    await app.listen(port, () => {
-      Logger.log(`🔒 HTTPS server is up and running on port ${port} in production environment!`);
-    });
-  }
+  Logger.log(`🚀 Server is up and running!`);
 };
 
 bootstrap();
