030bf98032cb8cb43095098a07c206922f0d451c
omerjadoon <omerjadoon1@gmail.com>
2023-12-09 06:55:55 +0100

    backend of chatGPT

--- server/src/scrap/scrap.service.ts
+++ server/src/scrap/scrap.service.ts
@@ -130,7 +130,7 @@ console.log("scrap service")
           model: 'gpt-3.5-turbo',
           messages: [
             { role: 'system', content: 'You are a helpful resume assistant.' },
-            { role: 'user', content: "give me 5 tips on resume" },
+            { role: 'user', content: input },
           ],
         },
         {
